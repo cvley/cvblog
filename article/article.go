a9a4e4cd051d77a96aa7760518b63709367afb83
cvley <ley@hackcv.com>
2017-03-30 23:09:03 +0800

    add article parser

--- article/article.go
+++ article/article.go
@@ -2,47 +2,67 @@ package article
 
 import (
 	"bytes"
+	"html/template"
 	"regexp"
 	"strings"
+	"time"
+
+	"github.com/cvley/cvblog/markdown"
 )
 
 var (
-	reDate   *regexp.Regexp
-	reTitle  *regexp.Regexp
-	reTag    *regexp.Regexp
-	reStatus *regexp.Regexp
-	reURL    *regexp.Regexp
+	reDate     *regexp.Regexp
+	reTitle    *regexp.Regexp
+	reCategory *regexp.Regexp
+	reTag      *regexp.Regexp
+	reStatus   *regexp.Regexp
+	reURL      *regexp.Regexp
 )
 
 type Article struct {
-	Date   string
-	Title  string
-	Tags   []string
-	Status string
-	URL    string
-	Body   []byte
+	Time     time.Time
+	Date     string
+	Title    template.HTML
+	Category string
+	Tags     []string
+	Status   string
+	URL      string
+	Body     template.HTML
 }
+
+type ArticleSortByTime []Article
 
 func init() {
 	reDate = regexp.MustCompile(`^Date: (.+)$`)
 	reTitle = regexp.MustCompile(`^Title: (.+)$`)
+	reCategory = regexp.MustCompile(`^Category: (.+)$`)
 	reTag = regexp.MustCompile(`^Tags: (.+)$`)
 	reStatus = regexp.MustCompile(`^Status: (.+)$`)
 	reURL = regexp.MustCompile(`^URL: (.+)$`)
 }
-func New(input []byte) *Article {
+
+func New(input []byte) Article {
 	content := bytes.SplitN(input, []byte("\n\n"), 2)
 
-	result := &Article{Body: content[1]}
+	result := Article{Body: template.HTML(markdown.Render(content[1]))}
 	prefixs := bytes.Split(content[0], []byte("\n"))
 	for _, prefix := range prefixs {
 		if reDate.Match(prefix) {
 			date := reDate.FindSubmatch(prefix)
-			result.Date = string(date[1])
+			t, err := time.Parse("2006-01-02 15:04", string(date[1]))
+			if err != nil {
+				t = time.Now()
+			}
+			result.Time = t
+			result.Date = t.Format("2006-01-02 15:04")
 		}
 		if reTitle.Match(prefix) {
 			title := reTitle.FindSubmatch(prefix)
-			result.Title = string(title[1])
+			result.Title = template.HTML(title[1])
+		}
+		if reCategory.Match(prefix) {
+			category := reCategory.FindSubmatch(prefix)
+			result.Category = string(category[1])
 		}
 		if reTag.Match(prefix) {
 			tags := reTag.FindSubmatch(prefix)
@@ -59,4 +79,25 @@ func New(input []byte) *Article {
 	}
 
 	return result
+}
+
+func (a Article) Summary() string {
+	length := len(a.Body)
+	if length > 500 {
+		length = 500
+	}
+
+	return string(a.Body)[:length]
+}
+
+func (byTime ArticleSortByTime) Len() int {
+	return len(byTime)
+}
+
+func (byTime ArticleSortByTime) Swap(i, j int) {
+	byTime[i], byTime[j] = byTime[j], byTime[i]
+}
+
+func (byTime ArticleSortByTime) Less(i, j int) bool {
+	return byTime[i].Time.Unix() > byTime[j].Time.Unix()
 }
--- article/article_test.go
+++ article/article_test.go
@@ -1,6 +1,7 @@
 package article
 
 import (
+	"sort"
 	"testing"
 )
 
@@ -12,4 +13,22 @@ func TestNew(t *testing.T) {
 	t.Log(paper.URL)
 	t.Log(paper.Date)
 	t.Log(paper.Status)
+	t.Log(paper.Summary())
+}
+
+func TestSortByDate(t *testing.T) {
+	input := []string{
+		"Date: 2012-10-25 12:22\nTitle: 我是文章的标题2\nCategory: Test\nTags: 标签1, 标签2\nStatus: draft\nURL: this-is-my-first-post\n\n然后开始写正文...",
+		"Date: 2012-10-25 13:22\nTitle: 我是文章的标题1\nTags: 标签1, 标签2\nStatus: draft\nURL: this-is-my-first-post\n\n然后开始写正文...",
+	}
+
+	articles := []Article{}
+	for _, v := range input {
+		paper := New([]byte(v))
+		articles = append(articles, paper)
+	}
+
+	sort.Sort(ArticleSortByTime(articles))
+	t.Log(articles[0].Date)
+	t.Log(articles[0].Title)
 }
